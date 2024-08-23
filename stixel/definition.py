47d4443683c33ce4e6cca9efd7e209c04e5bc7ef
MarcelVSHNS <54577510+MarcelVSHNS@users.noreply.github.com>
2024-08-23 17:16:03 +0200

    added binary function

--- stixel/definition.py
+++ stixel/definition.py
@@ -6,10 +6,13 @@ StixelWorld is the normal operating object, which contains Stixel
 from __future__ import annotations
 from typing import List, Tuple, Optional, Dict, Union
 from os import PathLike, path
+import pickle
 import numpy as np
 import pandas as pd
 from PIL import Image
-from .helper import _uvd_to_xyz
+from pyrealsense2 import camera_info
+
+from .helper import _uvd_to_xyz, CameraInfo
 
 
 class Stixel:
@@ -46,7 +49,7 @@ class Stixel:
         self.p = prob
 
     def convert_to_pseudo_coordinates(self,
-                                      camera_calib: Dict[str, np.array],
+                                      camera_calib: CameraInfo,
                                       image: Optional[Image] = None
                                       ) -> Tuple[List[np.ndarray], List[np.ndarray]]:
         """ Converts Stixel into a cartesian coordinates.
@@ -73,6 +76,20 @@ class Stixel:
                 colors.append(np.array([r / 255.0, g / 255.0, b / 255.0]))
         return coordinates, colors
 
+    def to_bytes(self) -> bytes:
+        """ Serialize the Stixel object to a bytes representation using pickle. """
+        return pickle.dumps(self)
+
+    @classmethod
+    def from_bytes(cls, data: bytes) -> 'Stixel':
+        """ Deserialize the bytes data back into a Stixel object.
+        Args:
+            data: bytes representation of the Stixel object
+        Returns:
+            Stixel object
+        """
+        return pickle.loads(data)
+
 
 class StixelWorld:
     """ A representation of a Scene with Stixel.
@@ -82,22 +99,44 @@ class StixelWorld:
 
     def __init__(self,
                  stixel_list: List[Stixel],
-                 img_name: str = "",
+                 img_name: Optional[str] = None,
                  image: Optional[Image] = None,
-                 camera_mtx: Optional[np.array] = None):
+                 cam_info: CameraInfo = CameraInfo()):
         self.stixel = stixel_list
-        self.image_name = img_name
+        self.camera_info = cam_info
         self.image = image
-        self.camera_mtx = camera_mtx
-        self.trans_mtx: np.array = np.zeros(3)
-        self.proj_mtx: Optional[np.array] = None
-        self.rect_mtx: np.array = np.eye(3)
+        if img_name is not None:
+            self.camera_info.img_name = img_name
 
     def __getattr__(self, attr) -> List[Stixel]:
         """ Enables direct access to attributes of the `stixel-list` object. """
         if hasattr(self.stixel, attr):
             return getattr(self.stixel, attr)
         raise AttributeError(f"'{type(self).__name__}' object has no attribute '{attr}'")
+
+    @property
+    def image(self):
+        return self._image
+
+    @image.setter
+    def image(self, img: Image) -> None:
+        self._image = img
+        if img is not None:
+            self.camera_info.img_size = img.size
+
+    @property
+    def camera_info(self):
+        return self._camera_info
+
+    @camera_info.setter
+    def camera_info(self, cam_info: CameraInfo) -> None:
+        merged_cam_info = cam_info
+        if hasattr(self, 'camera_info'):
+            merged_cam_info.img_size = (
+                self.camera_info.img_size if merged_cam_info.img_size is None else merged_cam_info.img_size)
+            merged_cam_info.img_name = (
+                self.camera_info.img_name if merged_cam_info.img_name is None else merged_cam_info.img_name)
+        self._camera_info = merged_cam_info
 
     @classmethod
     def read(cls, filepath: str | PathLike[str],
@@ -113,75 +152,142 @@ class StixelWorld:
         Returns:
 
         """
-        stixel_file_df: pd.DataFrame = pd.read_csv(filepath)
-        if translation_dict is not None or 'x' in stixel_file_df.columns:
-            if translation_dict is not None:
-                stixel_file_df = stixel_file_df.rename(columns=translation_dict)
-            else:
-                # compatibility to old format: img_path, x, yT, yB, class, depth
-                stixel_file_df = stixel_file_df.rename(
-                    columns={'img_path': 'img', 'x': 'u', 'yT': 'vT', 'yB': 'vB', 'depth': 'd'})
+        if filepath.endswith(".csv"):
+            stixel_file_df: pd.DataFrame = pd.read_csv(filepath)
+            if translation_dict is not None or 'x' in stixel_file_df.columns:
+                if translation_dict is not None:
+                    stixel_file_df = stixel_file_df.rename(columns=translation_dict)
+                else:
+                    # compatibility to old format: img_path, x, yT, yB, class, depth
+                    stixel_file_df = stixel_file_df.rename(
+                        columns={'img_path': 'img', 'x': 'u', 'yT': 'vT', 'yB': 'vB', 'depth': 'd'})
 
-        stixel_world_list: Optional[List[Stixel]] = []
-        img_name: str = path.basename(filepath)
-        for _, data in stixel_file_df.iterrows():
-            stixel = Stixel(u=data['u'],
-                            v_b=data['vB'],
-                            v_t=data['vT'],
-                            d=data['d'])
-            # Additional Infos
-            if stx_width is not None:
-                stixel.width = stx_width
-            if 'label' in data:
-                stixel.label = data['label']
-            if 'p' in data:
-                stixel.p = data['p']
-            img_name = path.basename(data['img'])
-            stixel_world_list.append(stixel)
+            stixel_world_list: Optional[List[Stixel]] = []
+            img_name: str = path.basename(filepath)
+            for _, data in stixel_file_df.iterrows():
+                stixel = Stixel(u=data['u'],
+                                v_b=data['vB'],
+                                v_t=data['vT'],
+                                d=data['d'])
+                # Additional Infos
+                if stx_width is not None:
+                    stixel.width = stx_width
+                if 'label' in data:
+                    stixel.label = data['label']
+                if 'p' in data:
+                    stixel.p = data['p']
+                img_name = path.basename(data['img'])
+                stixel_world_list.append(stixel)
+            return cls(stixel_world_list, img_name=img_name)
+        elif filepath.endswith(".stx1"):
+            with open(filepath, 'rb') as file:
+                return cls.from_bytes(file.read())
+        else:
+            raise Exception(f"File ending {path.splitext(filepath)[1]} not known. Current support: .csv and .stx1")
 
-        return cls(stixel_world_list, img_name=img_name)
-
-    def save(self, filepath: str | PathLike[str], filename: Optional[str] = None) -> None:
+    def save(self, filepath: str | PathLike[str] = "", filename: Optional[str] = None,
+             binary: bool = False) -> None:
         """
 
         Args:
             filepath:
             filename:
+            binary:
         """
-        target_list = []
-        for stixel in self.stixel:
-            target_list.append([f"{self.image_name}",
-                                int(stixel.u),
-                                int(stixel.vB),
-                                int(stixel.vT),
-                                round(stixel.d, 2),
-                                round(stixel.p, 2),
-                                int(stixel.label)])
-        target: pd.DataFrame = pd.DataFrame(target_list)
-        target.columns = ['img', 'u', 'vB', 'vT', 'd', 'p', 'label']
-        name = path.splitext(self.image_name)[0] if filename is None else filename
-        target.to_csv(path.join(filepath, name + ".csv"), index=False)
-        print(f"Saved Stixel: {name} to: {filepath}.")
+        name = path.splitext(self.camera_info.img_name)[0] if filename is None else filename
+        if binary:
+            file_path = path.join(filepath, name + ".stx1")
+            with open(file_path, 'wb') as file:
+                file.write(self.to_bytes())
+        else:
+            target_list = []
+            for stixel in self.stixel:
+                target_list.append([f"{self.image_name}",
+                                    int(stixel.u),
+                                    int(stixel.vB),
+                                    int(stixel.vT),
+                                    round(stixel.d, 2),
+                                    round(stixel.p, 2),
+                                    int(stixel.label)])
+            target: pd.DataFrame = pd.DataFrame(target_list)
+            target.columns = ['img', 'u', 'vB', 'vT', 'd', 'p', 'label']
+            target.to_csv(path.join(filepath, name + ".csv"), index=False)
+            print(f"Saved Stixel: {name} to: {filepath}. As CSV.")
 
-    def get_pseudo_coordinates(self) -> Union[Tuple[np.ndarray, np.ndarray], np.ndarray]:
+    def to_bytes(self, include_image: bool = True) -> bytes:
+        """ Serializes the StixelWorld object to a byte representation. """
+        # Serialize the Stixel list
+        stixel_bytes = pickle.dumps(self.stixel)
+        stixel_len = len(stixel_bytes).to_bytes(4, 'big')
+
+        # Serialize CameraInfo
+        if self.camera_info.K is None:
+            print(f"WARNING: Exporting data without camera intrinsics.")
+        cam_info_bytes = pickle.dumps(self.camera_info)
+        cam_info_len = len(cam_info_bytes).to_bytes(4, 'big')
+
+        # Serialize the Image (optional)
+        if include_image and self.image is not None:
+            img_bytes = self.image.tobytes()
+        else:
+            img_bytes = b''
+        img_len = len(img_bytes).to_bytes(4, 'big')
+
+        # Pack everything into a single byte stream
+        packed_data = stixel_len + stixel_bytes + cam_info_len + cam_info_bytes + img_len + img_bytes
+        return packed_data
+
+    @classmethod
+    def from_bytes(cls, data: bytes, name: str = "") -> "StixelWorld":
+        """ Deserializes the byte data back into a StixelWorld object. """
+        offset = 0
+        # Deserialize the Stixel list
+        stixel_len = int.from_bytes(data[offset:offset + 4], 'big')
+        offset += 4
+        stixel_list = pickle.loads(data[offset:offset + stixel_len])
+        offset += stixel_len
+
+        # Deserialize the CameraInfo length and CameraInfo bytes
+        cam_info_len = int.from_bytes(data[offset:offset + 4], 'big')
+        offset += 4
+        cam_info_bytes = data[offset:offset + cam_info_len]
+        camera_info: Optional[CameraInfo] = pickle.loads(cam_info_bytes)
+        offset += cam_info_len
+
+        # Deserialize the image length and image bytes
+        img_len = int.from_bytes(data[offset:offset + 4], 'big')
+        offset += 4
+        if img_len > 0:
+            assert camera_info is not None
+            img_bytes = data[offset:offset + img_len]
+            image = Image.frombytes('RGB', camera_info.img_size, img_bytes)
+            offset += img_len
+        else:
+            image = None
+
+        # Reconstruct the StixelWorld object
+        return cls(stixel_list=stixel_list,
+                   image=image,
+                   cam_info=camera_info)
+
+    def get_pseudo_coordinates(self, try_return_rgb: bool = True) -> Union[Tuple[np.ndarray, np.ndarray], np.ndarray]:
         """
+
+        Args:
+            try_return_rgb: option to return rgb values IF the image is available
 
         Returns:
 
         """
         # SNEAK PREVIEW
-        assert self.camera_mtx is not None, ("This function is just in combination with a camera "
-                                             "matrix available.")
-        camera_calib: Dict[str, np.array] = {"K": self.camera_mtx,
-                                             "R": self.rect_mtx,
-                                             "T": self.trans_mtx,
-                                             "P": self.proj_mtx}
+        assert self.camera_info is not None, ("This function is just in combination with a camera "
+                                              "matrix available.")
         coordinates = []
         colors = []
         for stixel in self.stixel:
-            stixel_pts, pts_colors = stixel.convert_to_pseudo_coordinates(camera_calib, self.image)
+            stixel_pts, pts_colors = stixel.convert_to_pseudo_coordinates(self.camera_info, self.image)
             coordinates.extend(stixel_pts)
             colors.extend(pts_colors)
-        if self.image is not None:
+        if self.image is not None and try_return_rgb:
             return np.array(coordinates), np.array(colors)
         return np.array(coordinates)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import open3d as o3d
-from stixel import StixelWorld
+from stixel import StixelWorld, CameraInfo
 from stixel.utils import draw_stixels_on_image
 from PIL import Image
 import yaml
@@ -9,12 +9,23 @@ import yaml
 def main():
     stixel_world: StixelWorld = StixelWorld.read("sample/10084636266401282188_1120_000_1140_000_0.csv")
     stixel_world.image = Image.open("sample/10084636266401282188_1120_000_1140_000_0.png")
+    # install pyyaml
     with open('sample/waymo_calib.yaml') as yaml_file:
         calib = yaml.load(yaml_file, Loader=yaml.FullLoader)
+    stixel_world.camera_info.K = np.array(calib['K'])
 
+    """ packing test """
+    stixel_world.save(binary=True)
+    stixel_world = StixelWorld.read("10084636266401282188_1120_000_1140_000_0.stx1")
+
+    """ visual 2d test """
     stixel_img = draw_stixels_on_image(stixel_world.image, stixel_world.stixel)
     stixel_img.show()
-    stixel_world.camera_mtx = np.array(calib['K'])
+    print(stixel_world.camera_info.K)
+    print(stixel_world.camera_info.img_size)
+
+    """ 3d test """
+    # install open3d
     stxl_wrld_pts, colors = stixel_world.get_pseudo_coordinates()
 
     point_cloud = o3d.geometry.PointCloud()
