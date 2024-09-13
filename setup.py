f402c78a57c37d6b0cc68f4c6d4fc81ca607295d
MarcelVSHNS <54577510+MarcelVSHNS@users.noreply.github.com>
2024-09-13 17:33:58 +0200

    changed convert_to_matrix to a numpy dtype array

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='pyStixel-lib',
-    version='0.5.7',
+    version='0.5.8',
     packages=find_packages(),
     include_package_data=True,
     package_data={
