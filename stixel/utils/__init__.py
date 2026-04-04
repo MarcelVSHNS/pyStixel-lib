be932e1209a422efdab6211f127af8eb3c036df1
Marcel <marcel.vosshans@hs-esslingen.de>
2026-04-04 22:39:43 +0200

    inits adapted

--- stixel/utils/__init__.py
+++ stixel/utils/__init__.py
@@ -1,6 +1,12 @@
 from .visualization import draw_stixels_on_image, draw_stixels_in_3d, visualize_stixels_and_3d_bboxes, draw_bbox_on_image, draw_depth_map_from_stixel_world
 from .packing import read, decode_img, read_csv, save, add_image, add_config_entry
-from .transformation import convert_to_point_cloud, convert_to_matrix, convert_to_3d_stixel, derive_depth_map_from_stixel_world
+from .transformation import (
+    filter_stixels_by_confidence,
+    convert_to_point_cloud,
+    convert_to_matrix,
+    convert_to_3d_stixel,
+    derive_depth_map_from_stixel_world,
+)
 from .detection import (
     attach_dbscan_clustering,
     BoundingBox3D,
